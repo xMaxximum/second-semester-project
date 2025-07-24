1f26759eb4cf6f9ccec193e75a57230eb37f27bd
Timoteo Zorn <zornt.tin24@student.dhbw-heidenheim.de>
2025-07-24 19:16:10 +0200

    [feat:] added basic implementation for MQTT-Service

---
+++ Server/Server/Services/MqttService.cs
@@ -0,0 +1,44 @@
+using HiveMQtt;
+using HiveMQtt.Client;
+using HiveMQtt.Client.Options;
+
+namespace Server.Services
+{
+    public class MqttService: IHostedService
+    {
+        private readonly ILogger<MqttService> _logger;
+        private HiveMQClient _client;
+
+        public MqttService(ILogger<MqttService> logger)
+        {
+            _logger = logger;
+            var clientOptions = new HiveMQClientOptions 
+                { 
+                    Host = "broker.hivemq.com", 
+                    Port = 1883 
+                    // _clientOptions.UserName = "BikeUser";
+                    // _clientOptions.Password = "";
+                };
+            _client = new HiveMQClient(clientOptions);
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var connectedResult = await _client.ConnectAsync().ConfigureAwait(false);
+
+            _client.OnMessageReceived += (sender, args) =>
+            {
+                // Handle Message in args.PublishMessage
+                Console.WriteLine("Message Received: {}", args.PublishMessage.PayloadAsString);
+            };
+
+            _client.SubscribeAsync("topic/test").ConfigureAwait(false);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _client.UnsubscribeAsync("topic/test");
+            _client.DisconnectAsync();
+        }
+    }
+}
