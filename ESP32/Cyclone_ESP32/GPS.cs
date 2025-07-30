a15692bd01b3b7606b77825e7c66bfd13ae2720e
Leon Scharf <Leon.scharf@gmail.com>
2025-07-30 19:24:01 +0200

    First implemetation of the gps test inconclusive as gps module is not soldered yet

---
+++ ESP32/Cyclone_ESP32/GPS.cs
@@ -0,0 +1,73 @@
+﻿using Iot.Device.Common.GnssDevice;
+using nanoFramework.Hardware.Esp32;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Cyclone_ESP32 { 
+    public class gps
+    {
+        private static GenericSerialGnssDevice gpsModule;
+        public static void Setup()
+            {
+            // Some modules like ESP32 requires to setup serial pins
+            // Configure GPIOs 16 and 17 to be used in UART2 (that's refered as COM3)
+            Configuration.SetPinFunction(16, DeviceFunction.COM2_RX);
+            Configuration.SetPinFunction(17, DeviceFunction.COM2_TX);
+
+            // By default baud rate is 9600
+            Nmea0183Parser.AddParser(new TxtData());
+            gpsModule = new GenericSerialGnssDevice("COM2");
+            gpsModule.FixChanged += FixChanged;
+            gpsModule.LocationChanged += LocationChanged;
+            gpsModule.OperationModeChanged += OperationModeChanged;
+            gpsModule.ParsingError += ParsingError;
+            gpsModule.ParsedMessage += ParsedMessage;
+            gpsModule.UnparsedMessage += UnparsedMessage;
+
+            gpsModule.Start();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void UnparsedMessage(string message)
+        {
+            Console.WriteLine($"Received unparsed message: {message}");
+        }
+
+        private static void ParsedMessage(NmeaData data)
+        {
+            Console.WriteLine($"Received parsed message: {data.GetType()}");
+            if (data is TxtData txtData)
+            {
+                Console.WriteLine($"Received TXT message: {txtData.Text}, severity: {txtData.Severity}");
+            }
+        }
+
+        private static void ParsingError(Exception exception)
+        {
+            Console.WriteLine($"Received parsed error: {exception.Message}");
+        }
+
+        private static void OperationModeChanged(GnssOperation mode)
+        {
+            Console.WriteLine($"Received Operation Mode changed: {mode}");
+        }
+
+        private static void LocationChanged(Location position)
+        {
+            Console.WriteLine($"Received position changed: {position.Latitude},{position.Longitude}");
+        }
+
+        private static void FixChanged(Fix fix)
+        {
+            Console.WriteLine($"Received Fix changed: {fix}");
+        }
+        public static void TryGetCurrentPosition()
+        {
+            Console.WriteLine("lattitude: " + gpsModule.Location.Latitude.ToString());
+            Console.WriteLine("longitude: " + gpsModule.Location.Longitude.ToString()); 
+        }
+    }
+
+}
--- ESP32/Cyclone_ESP32/Program.cs
+++ ESP32/Cyclone_ESP32/Program.cs
@@ -1,18 +1,27 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System;
+﻿using System;
+using System.Text;
 using System.Threading;
-
 
 namespace Cyclone_ESP32
 {
-    public class Program
+    internal class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            // App must not return.
-            Thread.Sleep(Timeout.Infinite);
-        }        
+            // Initialize the GPS module
+            Thread gpsThread = new Thread(gps.Setup)
+            {
+                Priority = ThreadPriority.BelowNormal    // ensures the thread will not block process exit
+            };
+            gpsThread.Start();
+            // Keep the application running
+            Console.WriteLine("Press any key to exit...");
+            while (true)
+            {
+                Thread.Sleep(4000);
+                gps.TryGetCurrentPosition();
+            }
+            
+        }
     }
 }
