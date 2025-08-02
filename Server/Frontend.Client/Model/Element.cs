3525844ab313be181edbf02351db38684d800b82
Sarah Bornträger <borntraegers.tin24@student.dhbw-heidenheim.de>
2025-08-02 19:26:49 +0200

    [feat]: home logged in layout + kpi & chart tiles + activity table

---
+++ Server/Frontend.Client/Model/Element.cs
@@ -0,0 +1,11 @@
+﻿namespace Frontend.Client.Model
+{
+    public class Element
+    {
+        public DateTime Date { get; set; }
+        public double Distance { get; set; }  // in kilometers
+        public TimeSpan Duration { get; set; }  // duration of the ride
+        public double AverageSpeed { get; set; }  // km/h
+        public int ElevationGain { get; set; }  // in meters
+    }
+}
