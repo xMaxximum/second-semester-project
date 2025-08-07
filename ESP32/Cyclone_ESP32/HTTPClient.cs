fefd549b273b7e5e576598d4e807b6dec88a597a
Leon Scharf <Leon.scharf@gmail.com>
2025-08-07 10:46:57 +0200

    Add: Added Network handler and started http Client development

--- ESP32/Cyclone_ESP32/HTTPClient.cs
+++ ESP32/Cyclone_ESP32/HTTPClient.cs
@@ -1,9 +1,34 @@
 ﻿using System;
 using System.Text;
+using System.Net.Http;
 
 namespace Cyclone_ESP32
 {
     internal class HTTPClient
     {
+        private string url, username, password;
+        private int port;
+        private string authToken;
+
+        public HTTPClient(string url, int port, string username, string password)
+        {
+            this.url = url;
+            this.port = port;
+            this.username = username;
+            this.password = password;
+        }
+
+        private void Authenticate()
+        {
+            // Here you would typically make an HTTP request to authenticate and retrieve a token
+            // For demonstration purposes, we'll just set a dummy token
+            authToken = "dummyAuthToken";
+        }
+
+        private void buildPostData() { 
+            string jsonPayload = "{\"username\": \"user1\", \"password\": \"pass123\"}";
+            StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+        }
     }
 }
---
+++ ESP32/Cyclone_ESP32/NetworkHandler.cs
@@ -0,0 +1,66 @@
+﻿using nanoFramework.Networking;
+using System;
+using System.Device.Wifi;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+
+namespace Cyclone_ESP32
+{
+    internal class NetworkHandler
+    {
+        private string ssid;
+        private string password;
+        private bool connected = false;
+        private WifiAdapter wifiAdapter;
+
+        public NetworkHandler(string ssid, string password)
+        {
+            this.ssid = ssid;
+            this.password = password;
+            wifiAdapter = WifiAdapter.FindAllAdapters()[0];
+            if (!tryConnectWifi()) { 
+                Console.WriteLine("Failed to connect to WiFi network.");
+                while (!connected)
+                {
+                    Console.WriteLine("Retrying connection to WiFi network: " + ssid);
+                    Thread.Sleep(5000);
+                    connected = tryConnectWifi();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Connected to WiFi network: " + ssid);
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+
+        private bool tryConnectWifi()
+        {
+            wifiAdapter.ScanAsync();
+            Thread.Sleep(10000); // Wait for scan to complete
+            foreach (WifiAvailableNetwork network in wifiAdapter.NetworkReport.AvailableNetworks)
+            {
+                if (network.Ssid == ssid)
+                {
+                    wifiAdapter.Connect(ssid, WifiReconnectionKind.Automatic, password);
+                    connected = true;
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public bool IsConnected()
+        {
+            return connected && NetworkInterface.GetIsNetworkAvailable();
+        }
+
+        public bool sendData()
+        {
+
+            return false;
+        }
+    }
+}
