ab97c2f9a451cbecf25c011c15cdbc1fe8451bf4
Timoteo Zorn <zornt.tin24@student.dhbw-heidenheim.de>
2025-08-12 12:04:52 +0200

    [refactor:] add EF migrations for model changes

---
+++ Server/Server/Migrations/20250812071456_RemoveDeviceForeignKey.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDeviceForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SensorDataPackets_Devices_DeviceId",
+                table: "SensorDataPackets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_SensorDataPackets_Devices_DeviceId",
+                table: "SensorDataPackets",
+                column: "DeviceId",
+                principalTable: "Devices",
+                principalColumn: "DeviceId",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
