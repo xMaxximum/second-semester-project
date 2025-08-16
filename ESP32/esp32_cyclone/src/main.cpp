6b70da77059087dcb42b5e1ebe82da833671d96f
ShadowWolf <leon.scharf@gmail.com>
2025-08-16 14:50:01 +0200

    Add: added webserver from sample for testing, works

--- ESP32/esp32_cyclone/src/main.cpp
+++ ESP32/esp32_cyclone/src/main.cpp
@@ -6,6 +6,7 @@
 #include "string.h"
 #include "TinyGPS++.h"
 #include <Arduino.h>
+#include <EEPROM.h>
 
 
 
@@ -21,6 +22,7 @@ void setupWlan();
 void checkFirstTimeConfig();
 void setupSequence();
 
+#define setupMode true
 
 // interface used for sdcard (false is SPI)
 #define SDMMC true
@@ -74,11 +76,18 @@ gpsData gpsdata;
 
 void setup()
 {
+  if(setupMode){
+    EEPROM.begin(128);
+    EEPROM.write(0, 0); // Mark setup as not done
+    EEPROM.commit();
+    EEPROM.end();
+  }
   Serial.begin(115200);
   // wait for serial monitor to connect
   delay(8000);
 
-  setupFileSystem();
+  //setupFileSystem();
+  checkFirstTimeConfig();
   setupWlan();
   setupGPS();
 
@@ -98,7 +107,7 @@ void loop()
   getSpeed();
   readGPSData();
 
-  dtTo200ms = currentTime - lastReadTime200ms;
+  dtTo200ms = currentTime - lastReadTime200ms;;
   if (dtTo200ms >= 200)
   {
     bufferCount++;
@@ -321,8 +330,9 @@ void updateAllData(){
 
 
 void checkFirstTimeConfig(){
-  file = SD.open("/isSetup.flag", FILE_READ);
-  if(!file){
+  EEPROM.begin(128);
+  
+  if(EEPROM.read(0) != 1){
     setupSequence();
   }
   else{
@@ -334,7 +344,85 @@ void checkFirstTimeConfig(){
 void setupSequence(){
   Serial.println("Running setup sequence...");
   WiFi.softAP("CycloneSetup", "INF2024AI");
+  WiFiServer server(80);
+  server.begin();
+  String header = "";
+  unsigned long previousTime = millis();
+  unsigned long timeoutTime = 300000; // 30 seconds timeout for setup
+  while(true){
+    WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>GPIO 26 - State </p>");
+            // If the output26State is off, it displays the ON button       
+            if (true) {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 27  
+            client.println("<p>GPIO 27 - State </p>");
+            // If the output27State is off, it displays the ON button       
+            if (false) {
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  
+      }
+    }
   
 
-
+  EEPROM.write(0, 1); // Mark setup as done
+  EEPROM.commit();
+  Serial.println("Setup complete. You can now connect to the CycloneSetup WiFi network.");
 }
